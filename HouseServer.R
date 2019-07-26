adcf463dce9678ca194675cbcce5057d42b63d5d
Chris Perrins <christopher.perrins@qa.com>
2019-07-26 10:09:09 +0100

    houses

---
+++ HouseServer.R
@@ -0,0 +1,52 @@
+#install.packages("shiny")
+library(shiny)
+options(scipen = 9999)
+
+?sidebarLayout
+
+ui <- fluidPage(
+    titlePanel("Hello Shiny!"),
+  
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      numericInput(inputId = "beds", label = "Number of Beds", value = 0, min = 0, step = 1),
+      sliderInput(inputId = "toilets", label = "Number of Toilets", min = 0, value = 0, step = 1, max = 10),
+      selectInput(inputId = "stories", label = "Number of Stories", choices = c(1:5))
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      textOutput(outputId = "Predicted"),
+      plotOutput(outputId = "Plot")
+    )
+  )
+)
+
+cleverCleverLogic <- function(input) {
+  bedsValue <- input$beds*100000
+  toiletsValue <- input$toilets*50000
+  storiesValue <- (as.numeric(input$stories)-1)*50000
+  value <- (bedsValue + toiletsValue + storiesValue)
+  return(value)
+}
+
+server <- function(input, output) {
+  
+  
+  output$Predicted <- renderText({
+    paste("Predicted Value of £", cleverCleverLogic(input))
+  })
+  
+  output$Plot <- renderPlot({
+    plot(c(1:20), c(1:20))
+  }
+  
+  )
+}
+
+shinyApp(ui = ui, server = server)
